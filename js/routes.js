9b7eb7a26fd81357feb01438694582a42b9694fd
Brian <fredyfontaine@yahoo.co.uk>
2016-06-17 02:17:22 +0100

    add heritage masonry to list of projects

--- js/routes.js
+++ js/routes.js
@@ -4,6 +4,7 @@
     $routeProvider
         .when('/almax', { templateUrl: 'accordionSections/partials/projects/almax.html' })
         .when('/metro', { templateUrl: 'accordionSections/partials/projects/metro.html' })
+        .when('/hm', { templateUrl: 'accordionSections/partials/projects/hm.html' })
         .when('/test', { template: '<h3>Test Page</h3>' })
 
     .otherwise({ templateUrl: 'accordionSections/partials/projects/almax.html' });
